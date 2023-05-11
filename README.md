8324dda3dbda504580a4980f02ec9a18ef9f8cc5
Alexey Kozyrev <guper095@gmail.com>
2023-05-11 21:58:42 +0300

    feat: Скопировать проект

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
-# Здесь будет ваш проект на Реакте с авторизацией и регистрацией
+# Проектная работа [**Mesto**](https://kozyrevalexey.github.io/mesto-react/)
+## Курс обучения в [**Яндекс Практикум**](https://practicum.yandex.ru/) по программе [**Веб-разработчик**](https://practicum.yandex.ru/web/)
+---
+<p align="center">
+<img src="https://i.ibb.co/VV7dm7M/mesto.jpg" alt="Логотип проекта" >
+</p>
 
-Используйте ваши предыдущие наработки по проекту Mesto. Это может быть работа 11-го спринта или его улучшенная версия после 2-х последних спринтов. 
+---
+# Цель текущего проекта, закрепить навыки работы с REACT <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" title="react" width="25" height="25"/>
 
-Все запросы на авторизацию, регистрацию и проверку токена должны работать через сервис `https://auth.nomoreparties.co`. Остальные запросы, не относящиеся к этой проектной работе могут быть к бэкенду из предыдущих спринтов.
+---
+> ### ...и это только начало, дальше будет еще интереснее, т.к. будут использоватся новые технологии, инструменты и приемы.
+ ### Результат моих трудов будет отображаться :point_right: [**тут**](https://kozyrevalexey.github.io/mesto-react/)
 
-Успехов!
+### Моя статистика
+
+![крайний commit](https://img.shields.io/github/last-commit/KozyrevAlexey/mesto?color=blue)
+![GitHub top language](https://img.shields.io/github/languages/top/KozyrevAlexey/mesto)
+![GitHub language count](https://img.shields.io/github/languages/count/KozyrevAlexey/mesto)
---
+++ src/component/App.js
@@ -0,0 +1,169 @@
+import React, {useEffect, useState} from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Footer from "./Footer";
+import ImagePopup from "./ImagePopup";
+import EditProfilePopup from "./EditProfilePopup";
+import EditAvatarPopup from "./EditAvatarPopup";
+import AddPlacePopup from "./AddPlacePopup";
+
+import { CurrentUserContext } from "../contexts/CurrentUserContext";
+import { api } from "../utilis/api";
+
+function App() {
+  const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = useState(false);
+  const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = useState(false);
+  const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = useState(false);
+  const [isImagePopupOpen, setIsImagePopupOpen] = useState(false);
+  const [selectedCard, setSelectedCard] = useState({});
+  const [currentUser, setCurrentUser] = useState({});
+  const [cards, setCards] = useState([]);
+  const [isPreloading, setIsPreloading] = useState(false);
+
+  useEffect(() => {
+    Promise.all([api.getUserInfoApi(), api.getInitialCards()])
+      .then(([user, card]) => {
+        setCurrentUser(user);
+        setCards(card);
+      })
+      .catch((err) => alert(err))
+  }, []);
+
+  function handleEditAvatarClick() {
+    setIsEditAvatarPopupOpen(true);
+  }
+
+  function hendleEditProfileClick() {
+    setIsEditProfilePopupOpen(true);
+  }
+
+  function hendleAddPlaceClick() {
+    setIsAddPlacePopupOpen(true);
+  }
+
+  function closeAllPopups() {
+    setIsEditAvatarPopupOpen(false);
+    setIsEditProfilePopupOpen(false);
+    setIsAddPlacePopupOpen(false);
+    setIsImagePopupOpen(false);
+  }
+
+  function handleCardClick(card) {
+    setSelectedCard(card);
+    setIsImagePopupOpen(true);
+  }
+
+  function handleCardLike(card) {
+    const isLiked = card.likes.some(i => i._id === currentUser._id);
+    api
+      .changeLikeCardStatus(card._id, isLiked)
+      .then((newCard) => {
+        setCards((state) =>
+          state.map((c) => c._id === card._id ? newCard : c));
+      })
+      .catch((err) => alert(err));
+  }
+
+  function handleCardDelete(card) {
+    api
+      .deleteCard(card._id)
+      .then(() => {
+        setCards(state => state.filter((c) => c._id !== card._id));
+      })
+      .catch((err) => alert(err))
+  }
+
+  function handleUpdateUser(value) {
+    setIsPreloading(true)
+    api
+      .setUserInfoApi(value)
+      .then((res) => {
+        setCurrentUser(res);
+        closeAllPopups();
+      })
+      .catch((err) => alert(err))
+      .finally(() => setIsPreloading(false))
+  }
+
+  function handleUpdateAvatar(value) {
+    setIsPreloading(true)
+    api
+      .setUserAvatar(value)
+      .then((res) => {
+        setCurrentUser(res);
+        closeAllPopups();
+      })
+      .catch((err) => alert(err))
+      .finally(() => setIsPreloading(false))
+  }
+
+  function handleAddPlaceSubmit(card) {
+    setIsPreloading(true);
+    api
+      .addNewCard(card)
+      .then((newCard) => {
+        setCards([newCard, ...cards]);
+        closeAllPopups();
+      })
+      .catch((err) => alert(err))
+      .finally(() => setIsPreloading(false))
+  }
+
+  function handleOverlayClose(evt) {
+    if (evt.target.classList.contains('popup')) {
+      closeAllPopups();
+    }
+  }
+
+  return (
+    <CurrentUserContext.Provider value={currentUser}>
+      <div className="page">
+        <Header />
+        <Main
+          onEditAvatar={handleEditAvatarClick}
+          onEditProfile={hendleEditProfileClick}
+          onAddPlace={hendleAddPlaceClick}
+          onCardClick={handleCardClick}
+          onCardLike={handleCardLike}
+          onCardDelete={handleCardDelete}
+          cards={cards}
+        />
+        <Footer />
+
+        <EditAvatarPopup
+          isOpen={isEditAvatarPopupOpen}
+          onClose={closeAllPopups}
+          onUpdateAvatar={handleUpdateAvatar}
+          isPreloading={isPreloading}
+          onOverlayClose={handleOverlayClose}
+        />
+
+        <EditProfilePopup
+          isOpen={isEditProfilePopupOpen}
+          onClose={closeAllPopups}
+          onUpdateUser={handleUpdateUser}
+          isPreloading={isPreloading}
+          onOverlayClose={handleOverlayClose}
+        />
+
+        <AddPlacePopup
+          isOpen={isAddPlacePopupOpen}
+          onClose={closeAllPopups}
+          onAddPlace={handleAddPlaceSubmit}
+          isPreloading={isPreloading}
+          onOverlayClose={handleOverlayClose}
+        />
+
+        <ImagePopup
+          card={selectedCard}
+          isOpen={isImagePopupOpen}
+          onClose={closeAllPopups}
+          onOverlayClose={handleOverlayClose} />
+        {/* </ImagePopup> */}
+
+      </div>
+    </CurrentUserContext.Provider>
+  )
+}
+
+export default App;
